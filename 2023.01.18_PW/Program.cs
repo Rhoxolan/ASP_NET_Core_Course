f7ace7086b1653c77d1d42f579f37d43f35f3fc2
Rhoxolan <pba17mail@gmail.com>
2023-02-09 10:11:56 +0200

    The second practical work was supplemented

--- 2023.01.18_PW/Program.cs
+++ 2023.01.18_PW/Program.cs
@@ -1,8 +1,19 @@
 WebApplication app = WebApplication.CreateBuilder(args).Build();
+ulong num;
 
-app.Run(async context =>
+app.Use(async (context, next) =>
 {
-    
+    //Добавить потом обработку ошибок, попробовать с трай парсе
+    //Попробовать юинт128
+    num = Convert.ToUInt64(context.Request.Query["number"]);
+    if(num == 0 || num == 1)
+    {
+        await context.Response.WriteAsync(num.ToString());
+    }
+    else
+    {
+        await next.Invoke();
+    }
 });
 
 app.Run();
