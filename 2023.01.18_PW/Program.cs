bae2adbcc4c1538e089a03e2c29eac45a3de09ce
Rhoxolan <pba17mail@gmail.com>
2023-02-09 12:21:22 +0200

    The second practical work was supplemented

--- 2023.01.18_PW/Program.cs
+++ 2023.01.18_PW/Program.cs
@@ -1,19 +1,24 @@
+using _2023._01._18_PW;
+
 WebApplication app = WebApplication.CreateBuilder(args).Build();
-ulong num;
+
+//Удалить классы, попробовать с методами, переменную не передавать а каждый раз забирать из запроса.
 
 app.Use(async (context, next) =>
 {
     //Добавить потом обработку ошибок, попробовать с трай парсе
     //Попробовать юинт128
-    num = Convert.ToUInt64(context.Request.Query["number"]);
-    if(num == 0 || num == 1)
+    ulong num = Convert.ToUInt64(context.Request.Query["number"]);
+    if (num == 0 || num == 1)
     {
         await context.Response.WriteAsync(num.ToString());
     }
     else
     {
-        await next.Invoke();
+        await next.Invoke(context, num);;
     }
 });
+
+app.UseMiddleware<FibonacciNumberMiddleware>();
 
 app.Run();
