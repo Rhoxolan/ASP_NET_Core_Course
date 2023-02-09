003b74c21f35f8fefeb18e89a5ea65f24d068bc1
Rhoxolan <pba17mail@gmail.com>
2023-02-09 23:39:38 +0200

    The second practical work was supplemented

---
+++ 2023.01.18_PW/FirstFibonacciNumberMiddleware.cs
@@ -0,0 +1,79 @@
+﻿namespace _2023._01._18_PW
+{
+    public class FirstFibonacciNumberMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public FirstFibonacciNumberMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        //Разнести на несколько классов
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (string.IsNullOrWhiteSpace(context.Request.Query["number"]))
+            {
+                context.Response.StatusCode = 405;
+            }
+            else
+            {
+                if (!ushort.TryParse(context.Request.Query["number"], out ushort num))
+                {
+                    context.Response.StatusCode = 405;
+                }
+                else
+                {
+                    if (num == 0 || num == 1)
+                    {
+                        await context.Response.WriteAsync(num.ToString());
+                    }
+                    else
+                    {
+                        await _next(context);
+                    }
+                }
+            }
+        }
+    }
+
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await _next(context);
+            if (context.Response.StatusCode == 405)
+            {
+                await context.Response.WriteAsync("Bad request");
+            }
+        }
+    }
+
+    public class FibonacciMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public FibonacciMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            ushort num = Convert.ToUInt16(context.Request.Query["number"]);
+            List<UInt128> fl = new() { 0, 1 };
+            for (int i = 0; i < (num - 1); i++)
+            {
+                fl.Add(fl[^1] + fl[^2]);
+            }
+            await context.Response.WriteAsync(fl[^1].ToString());
+        }
+    }
+}
--- 2023.01.18_PW/Program.cs
+++ 2023.01.18_PW/Program.cs
@@ -1,66 +1,74 @@
+using _2023._01._18_PW;
+
 WebApplication app = WebApplication.CreateBuilder(args).Build();
 
-//Разобраться с маршрутизацией
-//Добавить проверку на наличие запроса на фибоначчи
-//Перенести методы в классы
-//Переделать по-нормальному (проверяюищй метод и метод фиббоначчи)
+//Разнести первый Middleware на подклассы
+//Проверить корректность
+//Создать методы расширения
 
 app.Map("/", () => "Hello!");
 
 app.Map("/fibonacci", appbuilder =>
 {
-    appbuilder.Use(async (context, next) =>
-    {
-        if (!ushort.TryParse(context.Request.Query["number"], out ushort num))
-        {
-            context.Response.StatusCode = 405;
-            await next.Invoke();
-            return;
-        }
-        if (num == 0 || num == 1)
-        {
-            await context.Response.WriteAsync(num.ToString());
-        }
-        else
-        {
-            await next.Invoke();
-        }
-    });
-
-    appbuilder.Use(async (context, next) =>
-    {
-        if (context.Response.StatusCode == 405)
-        {
-            await context.Response.WriteAsync("Number is uncorrect");
-            return;
-        }
-        await next.Invoke();
-        if (context.Response.StatusCode == 405)
-        {
-            await context.Response.WriteAsync("Number is uncorrect");
-        }
-    });
-
-    appbuilder.Use(async (HttpContext context, RequestDelegate next) => //Типы данных параметров функции указаны явно из-за необходимости явно определить, какая перегрузка метода Use используется.
-    {
-        ushort num = Convert.ToUInt16(context.Request.Query["number"]);
-        try
-        {
-            List<UInt128> fl = new()
-        {
-            0, 1
-        };
-            for (int i = 0; i < (num - 1); i++)
-            {
-                fl.Add(fl[^1] + fl[^2]);
-            }
-            await context.Response.WriteAsync(fl[^1].ToString());
-        }
-        catch
-        {
-            context.Response.StatusCode = 405;
-        }
-    });
+    appbuilder.UseMiddleware<ErrorHandlingMiddleware>();
+    appbuilder.UseMiddleware<FirstFibonacciNumberMiddleware>();
+    appbuilder.UseMiddleware<FibonacciMiddleware>();
 });
+
+//app.Map("/fibonacci", appbuilder =>
+//{
+//    appbuilder.Use(async (context, next) =>
+//    {
+//        if (!ushort.TryParse(context.Request.Query["number"], out ushort num))
+//        {
+//            context.Response.StatusCode = 405;
+//            await next.Invoke();
+//            return;
+//        }
+//        if (num == 0 || num == 1)
+//        {
+//            await context.Response.WriteAsync(num.ToString());
+//        }
+//        else
+//        {
+//            await next.Invoke();
+//        }
+//    });
+
+//    appbuilder.Use(async (context, next) =>
+//    {
+//        if (context.Response.StatusCode == 405)
+//        {
+//            await context.Response.WriteAsync("Number is uncorrect");
+//            return;
+//        }
+//        await next.Invoke();
+//        if (context.Response.StatusCode == 405)
+//        {
+//            await context.Response.WriteAsync("Number is uncorrect");
+//        }
+//    });
+
+//    appbuilder.Use(async (HttpContext context, RequestDelegate next) => //Типы данных параметров функции указаны явно из-за необходимости явно определить, какая перегрузка метода Use используется.
+//    {
+//        ushort num = Convert.ToUInt16(context.Request.Query["number"]);
+//        try
+//        {
+//            List<UInt128> fl = new()
+//        {
+//            0, 1
+//        };
+//            for (int i = 0; i < (num - 1); i++)
+//            {
+//                fl.Add(fl[^1] + fl[^2]);
+//            }
+//            await context.Response.WriteAsync(fl[^1].ToString());
+//        }
+//        catch
+//        {
+//            context.Response.StatusCode = 405;
+//        }
+//    });
+//});
 
 app.Run();
