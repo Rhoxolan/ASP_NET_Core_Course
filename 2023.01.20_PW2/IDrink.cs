3466791a29b3ccabbd9e70b1013e1320ada9f1bc
Rhoxolan <pba17mail@gmail.com>
2023-02-13 23:30:28 +0200

    The second practical work from the third lesson was began

---
+++ 2023.01.20_PW2/IDrink.cs
@@ -0,0 +1,36 @@
+﻿namespace _2023._01._20_PW2
+{
+    public interface IDrink
+    {
+        public string Name { get; }
+
+        public double Calories { get; }
+    }
+
+    public class Tea : IDrink
+    {
+        public string Name
+            => "Tea";
+
+        public double Calories
+            => 1;
+    }
+
+    public class Coffe : IDrink
+    {
+        public string Name
+            => "Coffe";
+
+        public double Calories
+            => 0.5;
+    }
+
+    public class MineralWater : IDrink
+    {
+        public string Name
+            => "Mineral water";
+
+        public double Calories
+            => 0;
+    }
+}
