beaeb4c6940cd992a8de296fd3101eec42a9265b
Rhoxolan <pba17mail@gmail.com>
2023-02-14 10:30:16 +0200

    The second practical work from the third lesson was supplemented

--- 2023.01.20_PW2/IDrink.cs
+++ 2023.01.20_PW2/IDrink.cs
@@ -33,4 +33,118 @@
         public double Calories
             => 0;
     }
+
+    public class DrinkService
+    {
+        private IEnumerable<IDrink> _drinks;
+
+        public DrinkService(IEnumerable<IDrink> drinks)
+        {
+            _drinks = drinks;
+        }
+
+        public IDrink? GetTea()
+        {
+            return _drinks.Where(d => d is Tea).FirstOrDefault();
+        }
+
+        public IDrink? GetCoffe()
+        {
+            return _drinks.Where(d => d is Coffe).FirstOrDefault();
+        }
+
+        public IDrink? GetMineralWoter()
+        {
+            return _drinks.Where(d => d is MineralWater).FirstOrDefault();
+        }
+    }
+
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await _next(context);
+            if (context.Response.StatusCode == 405)
+            {
+                await context.Response.WriteAsync("Bad request");
+            }
+        }
+    }
+
+    public class QueryCheckerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public QueryCheckerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (string.IsNullOrWhiteSpace(context.Request.Query["drink"]))
+            {
+                context.Response.StatusCode = 405;
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+    }
+
+    public class QueryDrinkCheckerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public QueryDrinkCheckerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string drinkQuery = context.Request.Query["drink"]!;
+            if (drinkQuery == "tea" || drinkQuery == "coffe" || drinkQuery == "mineralwater")
+            {
+                await _next(context);
+            }
+            else
+            {
+                context.Response.StatusCode = 405;
+            }
+        }
+    }
+
+    public class DrinkMiddelware
+    {
+        public DrinkMiddelware(RequestDelegate _)
+        {
+        }
+
+        public async Task InvokeAsync(HttpContext context, DrinkService drinkService)
+        {
+            IDrink? drink = default;
+            switch (context.Request.Query["drink"])
+            {
+                case "tea":
+                    drink = drinkService.GetTea();
+                    break;
+                case "coffe":
+                    drink = drinkService.GetTea();
+                    break;
+                case "mineralwater":
+                    drink = drinkService.GetMineralWoter();
+                    break;
+            }
+            await context.Response.WriteAsync($"The drink {drink!.Name} has {drink!.Calories} calories");
+        }
+    }
 }
