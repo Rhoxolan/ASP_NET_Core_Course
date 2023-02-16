e3bd0a0fcaf216aa40e8bd783c4dff775f765a22
Rhoxolan <pba17mail@gmail.com>
2023-02-16 21:32:10 +0200

    The fourth practical work was supplemented

---
+++ 2023.01.23_PW/Extensions/ConfigurationManagerExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace _2023._01._23_PW.Extensions
+{
+    public static class ConfigurationManagerExtensions
+    {
+        public static IConfigurationBuilder AddStudentConfigs(this ConfigurationManager configuration)
+            => configuration.AddJsonFile("student.json")
+            .AddXmlFile("student.xml");
+    }
+}
--- 2023.01.23_PW/Program.cs
+++ 2023.01.23_PW/Program.cs
@@ -3,8 +3,7 @@ using _2023._01._23_PW.Extensions;
 
 string[] commandLineArgs = { "color=red" }; //Command line args simulation
 var builder = WebApplication.CreateBuilder(commandLineArgs);
-builder.Configuration.AddJsonFile("student.json")
-    .AddXmlFile("student.xml");
+builder.Configuration.AddStudentConfigs();
 builder.Services.Configure<Student>(builder.Configuration.GetSection("Student"));
 
 var app = builder.Build();
