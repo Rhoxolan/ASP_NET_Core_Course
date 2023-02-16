3f41a93e918faa6ee3434756cab8d56b891a57cc
Rhoxolan <pba17mail@gmail.com>
2023-02-16 23:37:25 +0200

    The fourth practical work was supplemented

--- 2023.01.23_PW/Program.cs
+++ 2023.01.23_PW/Program.cs
@@ -2,6 +2,7 @@ using _2023._01._23_PW;
 using _2023._01._23_PW.Extensions;
 
 string[] commandLineArgs = { "color=red" }; //Command line args simulation
+
 var builder = WebApplication.CreateBuilder(commandLineArgs);
 builder.Configuration.AddStudentConfigs();
 builder.Services.Configure<Student>(builder.Configuration.GetSection("Student"));
