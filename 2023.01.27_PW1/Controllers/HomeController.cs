346f2339c62d2e1ea6ba04c536bb4c8e0b5fd8b6
Rhoxolan <pba17mail@gmail.com>
2023-02-20 11:52:01 +0200

    The first part from the sixth practical work was edited

--- 2023.01.27_PW1/Controllers/HomeController.cs
+++ 2023.01.27_PW1/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace _2023._01._27_PW1.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
