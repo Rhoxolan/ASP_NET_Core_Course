3d649385ab1c19d8ab82bbc34d52f8759fa8fbd4
Rhoxolan <pba17mail@gmail.com>
2023-02-21 10:15:08 +0200

    The first part from the sixth practical work was supplemented

--- 2023.01.27_PW1/Controllers/HomeController.cs
+++ 2023.01.27_PW1/Controllers/HomeController.cs
@@ -4,24 +4,45 @@ using System.Diagnostics;
 
 namespace _2023._01._27_PW1.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index(double? leftTopFractionInput, double? leftBottomFractionInput, double? rightTopFractionInput, double? rightBottomFractionInput)
+		{
+			if(leftTopFractionInput is not null)
+			{
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+			}
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		private double AddFractions(double leftTopFraction, double leftBottomFraction, double rightTopFraction, double rightBottomFraction)
+			=> (leftTopFraction * rightBottomFraction + rightTopFraction * leftBottomFraction) / (leftBottomFraction * rightBottomFraction);
+
+		private double SubtractFractions(double leftTopFraction, double leftBottomFraction, double rightTopFraction, double rightBottomFraction)
+			=> (leftTopFraction * rightBottomFraction - rightTopFraction * leftBottomFraction) / (leftBottomFraction * rightBottomFraction);
+
+		private double MultiplyFractions(double leftTopFraction, double leftBottomFraction, double rightTopFraction, double rightBottomFraction)
+			=> (leftTopFraction * rightTopFraction) / (leftBottomFraction * rightBottomFraction);
+
+		private double DivideFractions(double leftTopFraction, double leftBottomFraction, double rightTopFraction, double rightBottomFraction)
+			=> (leftTopFraction * rightBottomFraction) / (rightTopFraction * leftBottomFraction);
+
+		double PowerOfFraction(double leftTopFraction, double leftBottomFraction, double rightTopFraction, double rightBottomFraction)
+			=> Math.Pow(Math.Pow(leftTopFraction / leftBottomFraction, rightTopFraction / rightBottomFraction), 1 / rightBottomFraction);
+
+
+	}
 }
