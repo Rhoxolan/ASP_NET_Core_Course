bb15e5e4606032d90a724828dab895028f2b68ab
Rhoxolan <pba17mail@gmail.com>
2023-02-21 20:43:09 +0200

    The second part from the sixth practical work was began

---
+++ 2023.01.27_PW2/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _2023._01._27_PW2.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
