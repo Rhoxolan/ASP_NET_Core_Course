4dfad1b41845e2d3a162464ba2e38d302eb4a664
Rhoxolan <pba17mail@gmail.com>
2023-02-22 13:47:40 +0200

    The second part from the sixth practical work was supplemented

--- 2023.01.27_PW2/wwwroot/fractionsScript.js
+++ 2023.01.27_PW2/wwwroot/fractionsScript.js
@@ -2,12 +2,17 @@
     e.preventDefault();
     const fileInput = document.createElement('input');
     fileInput.type = 'file';
-    fileInput.click();
+    fileInput.accept = ".json";
     fileInput.addEventListener('change', () => {
-        let fractions = JSON.parse(fileInput.files[0]);
-        document.getElementById("leftTopFractionInput").innerText = fractions.leftTopFraction;
-        document.getElementById("leftBottomFractionInput").innerText = fractions.leftBottomFraction;
-        document.getElementById("rightTopFractionInput").innerText = fractions.rightTopFraction;
-        document.getElementById("rightBottomFractionInput").innerText = fractions.rightBottomFraction;
-    })
+        const reader = new FileReader();
+        reader.addEventListener('load', () => {
+            const fractions = JSON.parse(reader.result);
+            document.getElementById("leftTopFractionInput").value = fractions.leftTopFraction;
+            document.getElementById("leftBottomFractionInput").value = fractions.leftBottomFraction;
+            document.getElementById("rightTopFractionInput").value = fractions.rightTopFraction;
+            document.getElementById("rightBottomFractionInput").value = fractions.rightBottomFraction;
+        });
+        reader.readAsText(fileInput.files[0]);
+    });
+    fileInput.click();
 });
