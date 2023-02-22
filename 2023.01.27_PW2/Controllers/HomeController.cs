cbf083d80cfbe767dd272609839fbed7ac57c8b7
Rhoxolan <pba17mail@gmail.com>
2023-02-22 14:25:28 +0200

    The second part from the sixth practical work was finished

--- 2023.01.27_PW2/Controllers/HomeController.cs
+++ 2023.01.27_PW2/Controllers/HomeController.cs
@@ -4,9 +4,60 @@ namespace _2023._01._27_PW2.Controllers
 {
 	public class HomeController : Controller
 	{
+		[HttpPost]
+		public IActionResult Index(double leftTopFractionInput, double leftBottomFractionInput, double rightTopFractionInput, double rightBottomFractionInput)
+		{
+			double? res = default;
+			foreach (var formElem in HttpContext.Request.Form)
+			{
+				if (formElem.Value == "addition")
+				{
+					res = AddFractions(leftTopFractionInput, leftBottomFractionInput, rightTopFractionInput, rightBottomFractionInput);
+					break;
+				}
+				if (formElem.Value == "substraction")
+				{
+					res = SubtractFractions(leftTopFractionInput, leftBottomFractionInput, rightTopFractionInput, rightBottomFractionInput);
+					break;
+				}
+				if (formElem.Value == "multiplication")
+				{
+					res = MultiplyFractions(leftTopFractionInput, leftBottomFractionInput, rightTopFractionInput, rightBottomFractionInput);
+					break;
+				}
+				if (formElem.Value == "division")
+				{
+					res = DivideFractions(leftTopFractionInput, leftBottomFractionInput, rightTopFractionInput, rightBottomFractionInput);
+					break;
+				}
+				if (formElem.Value == "power")
+				{
+					res = PowerOfFraction(leftTopFractionInput, leftBottomFractionInput, rightTopFractionInput, rightBottomFractionInput);
+					break;
+				}
+			}
+			return View(res);
+		}
+
+		[HttpGet]
 		public IActionResult Index()
 		{
-			return View();
+			return View(null);
 		}
+
+		private double AddFractions(double leftTopFraction, double leftBottomFraction, double rightTopFraction, double rightBottomFraction)
+			=> (leftTopFraction * rightBottomFraction + rightTopFraction * leftBottomFraction) / (leftBottomFraction * rightBottomFraction);
+
+		private double SubtractFractions(double leftTopFraction, double leftBottomFraction, double rightTopFraction, double rightBottomFraction)
+			=> (leftTopFraction * rightBottomFraction - rightTopFraction * leftBottomFraction) / (leftBottomFraction * rightBottomFraction);
+
+		private double MultiplyFractions(double leftTopFraction, double leftBottomFraction, double rightTopFraction, double rightBottomFraction)
+			=> (leftTopFraction * rightTopFraction) / (leftBottomFraction * rightBottomFraction);
+
+		private double DivideFractions(double leftTopFraction, double leftBottomFraction, double rightTopFraction, double rightBottomFraction)
+			=> (leftTopFraction * rightBottomFraction) / (rightTopFraction * leftBottomFraction);
+
+		private double PowerOfFraction(double leftTopFraction, double leftBottomFraction, double rightTopFraction, double rightBottomFraction)
+			=> Math.Pow(Math.Pow(leftTopFraction / leftBottomFraction, rightTopFraction / rightBottomFraction), 1 / rightBottomFraction);
 	}
 }
--- 2023.01.27_PW2/wwwroot/fractionsScript.js
+++ 2023.01.27_PW2/wwwroot/fractionsScript.js
@@ -1,18 +1,24 @@
-﻿document.getElementById('addFromFileButton').addEventListener("click", e => {
+﻿document.getElementById('addFromFileButton').addEventListener("click", onAddFromFileButtonClick);
+
+function onAddFromFileButtonClick(e) {
     e.preventDefault();
     const fileInput = document.createElement('input');
     fileInput.type = 'file';
     fileInput.accept = ".json";
-    fileInput.addEventListener('change', () => {
-        const reader = new FileReader();
-        reader.addEventListener('load', () => {
-            const fractions = JSON.parse(reader.result);
-            document.getElementById("leftTopFractionInput").value = fractions.leftTopFraction;
-            document.getElementById("leftBottomFractionInput").value = fractions.leftBottomFraction;
-            document.getElementById("rightTopFractionInput").value = fractions.rightTopFraction;
-            document.getElementById("rightBottomFractionInput").value = fractions.rightBottomFraction;
-        });
-        reader.readAsText(fileInput.files[0]);
-    });
+    fileInput.addEventListener('change', () => onInputChange(fileInput));
     fileInput.click();
-});
+}
+
+function onInputChange(fileInput) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => onFileReaderLoad(reader));
+    reader.readAsText(fileInput.files[0]);
+}
+
+function onFileReaderLoad(reader) {
+    const fractions = JSON.parse(reader.result);
+    document.getElementById("leftTopFractionInput").value = fractions.leftTopFraction;
+    document.getElementById("leftBottomFractionInput").value = fractions.leftBottomFraction;
+    document.getElementById("rightTopFractionInput").value = fractions.rightTopFraction;
+    document.getElementById("rightBottomFractionInput").value = fractions.rightBottomFraction;
+}
