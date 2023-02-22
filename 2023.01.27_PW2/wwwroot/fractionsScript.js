ddf4f55f8e73226dac94aa0fc62e9edf1ba58f27
Rhoxolan <pba17mail@gmail.com>
2023-02-22 10:02:52 +0200

    The second part from the sixth practical work was supplemented

--- 2023.01.27_PW2/wwwroot/fractionsScript.js
+++ 2023.01.27_PW2/wwwroot/fractionsScript.js
@@ -1,1 +1,13 @@
-﻿//Написать обработчик для кнопки загрузки, перехватывающий отправку данных и подставляющий значения в ячейки с файла.
+﻿document.getElementById('addFromFileButton').addEventListener("click", e => {
+    e.preventDefault();
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.click();
+    fileInput.addEventListener('change', () => {
+        let fractions = JSON.parse(fileInput.files[0]);
+        document.getElementById("leftTopFractionInput").innerText = fractions.leftTopFraction;
+        document.getElementById("leftBottomFractionInput").innerText = fractions.leftBottomFraction;
+        document.getElementById("rightTopFractionInput").innerText = fractions.rightTopFraction;
+        document.getElementById("rightBottomFractionInput").innerText = fractions.rightBottomFraction;
+    })
+});
