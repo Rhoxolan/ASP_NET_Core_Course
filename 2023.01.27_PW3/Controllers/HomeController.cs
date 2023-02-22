9d016241c5f53aa146a41f272f21b1a15b7d19d8
Rhoxolan <pba17mail@gmail.com>
2023-02-22 20:59:34 +0200

    The third part from the sixth practical work was completed

---
+++ 2023.01.27_PW3/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using _2023._01._27_PW3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+
+namespace _2023._01._27_PW3.Controllers
+{
+	public class HomeController : Controller
+	{
+
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+		//Query example: /Home/CarInfo?Model=Accord&Color=Red&Mark=Honda&Year=2003&EngineDisplacement=2.1
+		public IActionResult CarInfo(Car car)
+		{
+			return View(car);
+		}
+
+		//Query example: /Home/CarFile?Model=Accord&Color=Red&Mark=Honda&Year=2003&EngineDisplacement=2.1
+		public IActionResult CarFile(Car car)
+		{
+			return File(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(car)), "application/json", "car.json");
+		}
+
+    }
+}
---
+++ 2023.01.27_PW3/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace _2023._01._27_PW3.Models
+{
+	public record Car()
+	{
+		public required string Model { get; set; }
+
+		public required string Color { get; set; }
+
+		public required string Mark { get; set; }
+
+		public required int Year { get; set; }
+
+		public required double EngineDisplacement { get; set; }
+	}
+}
