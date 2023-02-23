01b97c4c9ecca629350aaf1a8402e9777cd7adfc
Rhoxolan <pba17mail@gmail.com>
2023-02-23 12:42:41 +0200

    The fourth part from the sixth practical work was finished

--- 2023.01.27_PW4/Controllers/HomeController.cs
+++ 2023.01.27_PW4/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 using System.Text;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using static System.Environment;
 
 namespace _2023._01._27_PW4.Controllers
 {
@@ -16,16 +18,35 @@ namespace _2023._01._27_PW4.Controllers
 		//Query example: /Home/CarInfo?Model=Accord&Color=Red&Mark=Honda&Year=2003&EngineDisplacement=2.1
 		public IActionResult CarInfo(Car car)
 		{
-			//if(ModelState.IsValid)
-			//{
-				return View(car);
-			//}
+			ModelChecker(ModelState);
+			return View(car);
 		}
 
 		//Query example: /Home/CarFile?Model=Accord&Color=Red&Mark=Honda&Year=2003&EngineDisplacement=2.1
 		public IActionResult CarFile(Car car)
 		{
+			ModelChecker(ModelState);
 			return File(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(car)), "application/json", "car.json");
+		}
+
+		private void ModelChecker(ModelStateDictionary modelState)
+		{
+			if (!modelState.IsValid)
+			{
+				Console.WriteLine($"Query validation errors count: {ModelState.ErrorCount}");
+				StringBuilder stringBuilder = new();
+				foreach (var modelValue in ModelState.Values)
+				{
+					if (modelValue.ValidationState != ModelValidationState.Valid)
+					{
+						foreach (var error in modelValue.Errors)
+						{
+							stringBuilder.AppendLine(error.ErrorMessage);
+						}
+					}
+				}
+				Console.WriteLine($"Errors: {NewLine}{stringBuilder}");
+			}
 		}
 	}
 }
