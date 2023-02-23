7b1385ed97feed5b88ccb7e62c60ac6b3630b7fb
Rhoxolan <pba17mail@gmail.com>
2023-02-23 10:22:34 +0200

    The fourth part from the sixth practical work was began

---
+++ 2023.01.27_PW4/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using _2023._01._27_PW4.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text;
+
+namespace _2023._01._27_PW4.Controllers
+{
+	public class HomeController : Controller
+	{
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		//Query example: /Home/CarInfo?Model=Accord&Color=Red&Mark=Honda&Year=2003&EngineDisplacement=2.1
+		public IActionResult CarInfo(Car car)
+		{
+			//if(ModelState.IsValid)
+			//{
+				return View(car);
+			//}
+		}
+
+		//Query example: /Home/CarFile?Model=Accord&Color=Red&Mark=Honda&Year=2003&EngineDisplacement=2.1
+		public IActionResult CarFile(Car car)
+		{
+			return File(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(car)), "application/json", "car.json");
+		}
+	}
+}
---
+++ 2023.01.27_PW4/Models/Car.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace _2023._01._27_PW4.Models
+{
+	public record Car()
+	{
+		[BindRequired]
+		public required string Model { get; set; }
+
+		[BindRequired]
+		public required string Color { get; set; }
+
+		[BindRequired]
+		public required string Mark { get; set; }
+
+		[BindRequired]
+		public required int Year { get; set; }
+
+		[BindRequired]
+		public required double EngineDisplacement { get; set; }
+	}
+}
