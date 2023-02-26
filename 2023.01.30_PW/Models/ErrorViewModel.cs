cb93889cca2afc6fdceca2d5c0a082475ff4ff2b
Rhoxolan <pba17mail@gmail.com>
2023-02-26 10:28:23 +0200

    The seventh practical work was began

---
+++ 2023.01.30_PW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _2023._01._30_PW.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
