d6c8d3ddbf58a735b91419bb1910d0d2b742279f
Rhoxolan <pba17mail@gmail.com>
2023-03-14 23:26:22 +0200

    The Academy project from thirteenth lesson was supplemented

--- Project_From_Meeting_Of_13_02_2023/Controllers/AdminController.cs
+++ Project_From_Meeting_Of_13_02_2023/Controllers/AdminController.cs
@@ -19,5 +19,20 @@ namespace Project_From_Meeting_Of_13_02_2023.Controllers
 			int? id = Convert.ToInt32(RouteData.Values["id"]?.ToString());
 			return Content($"Controller: {controller} | Action: {action}, Id: {id}");
 		}
+
+		public IActionResult Delete(int? id)
+		{
+			string? controller = RouteData.Values["controller"]!.ToString();
+			string? action = RouteData.Values["action"]!.ToString();
+			int? routeId = Convert.ToInt32(RouteData.Values["id"]?.ToString());
+			string? queryId = Request.Query["id"];
+			string? formId = string.Empty;
+			//if (Request.Form.Count != 0 && Request.Form.ContainsKey("id"))
+			//{
+			//	formId = HttpContext.Request.Form["id"];
+			//}
+			return Content($"Controller: {controller} | Action: {action}, Id: {id}, Id From Query: {queryId}, " +
+				$"Id from Form: {formId}, Id from Route: {routeId}");
+		}
 	}
 }
