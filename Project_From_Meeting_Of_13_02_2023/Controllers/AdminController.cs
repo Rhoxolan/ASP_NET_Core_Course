dee7c1bb91546f33fb86ade8c8092bf3384e522d
Rhoxolan <pba17mail@gmail.com>
2023-03-14 21:20:18 +0200

    The Academy project from thirteenth lesson was began

---
+++ Project_From_Meeting_Of_13_02_2023/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_From_Meeting_Of_13_02_2023.Controllers
+{
+	public class AdminController : Controller
+	{
+		public IActionResult Index()
+		{
+			string controller = RouteData.Values["controller"]!.ToString()!;
+			string action = RouteData.Values["action"]!.ToString()!;
+			int? id = Convert.ToInt32(RouteData.Values["id"]?.ToString());
+			return Content($"Controller: {controller} | Action: {action}, Id: {id}");
+		}
+
+		public IActionResult Edit()
+		{
+			string controller = RouteData.Values["controller"]!.ToString()!;
+			string action = RouteData.Values["action"]!.ToString()!;
+			int? id = Convert.ToInt32(RouteData.Values["id"]?.ToString());
+			return Content($"Controller: {controller} | Action: {action}, Id: {id}");
+		}
+	}
+}
---
+++ Project_From_Meeting_Of_13_02_2023/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_From_Meeting_Of_13_02_2023.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
