8fb6c4ab13d7dbb64e6750e67bad43c332b1cf51
Rhoxolan <pba17mail@gmail.com>
2023-03-15 18:19:34 +0200

    Unuseless Academy project was removed

--- Project_From_Meeting_Of_13_02_2023/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Project_From_Meeting_Of_13_02_2023.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
