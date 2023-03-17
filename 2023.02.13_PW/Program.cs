43a86572112eb358079436e79cbd741c0e2003b1
Rhoxolan <pba17mail@gmail.com>
2023-03-17 17:43:42 +0200

    The practical work from the thirteenth lesson was finished

--- 2023.02.13_PW/Program.cs
+++ 2023.02.13_PW/Program.cs
@@ -12,9 +12,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -27,33 +27,33 @@ app.UseAuthorization();
 //Примечание: чем более специфичней маршрут - тем выше он расположен
 
 app.MapControllerRoute(
-	name: "newOrderRoute", //Task 1
-	pattern: "newOrder",
-	defaults: new { controller = "Shop", action = "Buy" });
+    name: "newOrderRoute", //Task 1
+    pattern: "newOrder",
+    defaults: new { controller = "Shop", action = "Buy" });
 
 app.MapControllerRoute(
-	name: "home", //Task 2
-	pattern: "{controller:regex(^H.*)=Home}/{action:maxlength(6)=index}/{id?}");
+    name: "home", //Task 2
+    pattern: "{controller:regex(^H.*)=Home}/{action:maxlength(6)=index}/{id?}");
 
 app.MapControllerRoute(
-	name: "usersettingsNewRoute", //Task 3
-	pattern: "usersettings/{id:range(1,999)}",
-	defaults: new { controller = "User", action = "Settings" });
+    name: "usersettingsNewRoute", //Task 3
+    pattern: "usersettings/{id:range(1,999)}",
+    defaults: new { controller = "User", action = "Settings" });
 
 app.MapControllerRoute(
-	name: "userDefaultRoute",
-	pattern: "user/{action}",
-	defaults: new {action="User"}
-	);
+    name: "userDefaultRoute",
+    pattern: "user/{action}/{id?}",
+    defaults: new { controller = "User" }
+    );
 
 app.MapControllerRoute(
-	name: "adminDefaultRoute", //Task 4
-	pattern: "admin/{action}",
-	defaults: new { controller = "Admin" }, //Примечание - необходимо указывать значение сегмента по умолчанию, если в паттерне маршрута используется статический сегмент.
-	constraints: new { action = new EndsSetupConstraint() });
+    name: "adminDefaultRoute", //Task 4
+    pattern: "admin/{action}",
+    defaults: new { controller = "Admin" }, //Примечание - необходимо указывать значение сегмента по умолчанию, если в паттерне маршрута используется статический сегмент.
+    constraints: new { action = new EndsSetupConstraint() });
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller:regex(^[^HAU].*)}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller:regex(^[^HAU].*)}/{action=Index}/{id?}");
 
 app.Run();
