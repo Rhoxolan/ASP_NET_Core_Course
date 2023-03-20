2114d8053079e8446bd4e3eaf0bdf2d72173190d
Rhoxolan <pba17mail@gmail.com>
2023-03-20 20:23:06 +0200

    The Cat Project From Academy was supplemented

---
+++ CatsProject/TagHelpers/BreedsTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using static System.String;
+
+namespace CatsProject.TagHelpers
+{
+	public class BreedsTagHelper : TagHelper
+	{
+		public IEnumerable<string> Breeds { get; set; } = default!;
+
+		public string BreedsClass { get; set; } = Empty;
+
+		public string AClass { get; set; } = Empty;
+
+		public string Ahref { get; set; } = Empty;
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "div";
+			output.Attributes.SetAttribute("class", BreedsClass);
+			foreach (var breed in Breeds)
+			{
+				output.Content.AppendHtml($"<a class='{AClass}' href='{Ahref}/{breed}'>{breed}</a>");
+			}
+		}
+	}
+}
