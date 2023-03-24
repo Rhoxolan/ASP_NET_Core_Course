204a7f4fb3f820aa0b09cdfdb8e1f1a97a312b1d
Rhoxolan <pba17mail@gmail.com>
2023-03-24 23:18:42 +0200

    The Online Shop Project from Academy was began

---
+++ OnlineShop/Data/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineShop.Data
+{
+	public class Category
+	{
+		public int Id { get; set; }
+
+		public string Title { get; set; } = default!;
+
+		public int? ParentCategoryId { get; set; }
+
+		public Category? ParentCategory { get; set; } = default!;
+	}
+}
---
+++ OnlineShop/Data/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineShop.Data
+{
+	public class Product
+	{
+		public int Id { get; set; }
+
+		public string Title { get; set; } = default!;
+
+		public double Price { get; set; }
+
+		public int Count { get; set; }
+	}
+}
