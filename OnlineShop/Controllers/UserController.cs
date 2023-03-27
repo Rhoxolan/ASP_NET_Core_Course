c54d05529cb2e16dc7a409c8066e802f9fc60e91
Rhoxolan <pba17mail@gmail.com>
2023-03-27 21:36:47 +0300

    The Online Shop project from Academy was supplemented

--- OnlineShop/Controllers/UserController.cs
+++ OnlineShop/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineShop.Data;
@@ -6,27 +7,104 @@ using OnlineShop.Models.DTO.UserDTOs;
 
 namespace OnlineShop.Controllers
 {
-	public class UserController : Controller
-	{
-		private readonly UserManager<User> _userManager;
+    //[Authorize(Roles = "admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> _userManager;
 
-		public UserController(UserManager<User> userManager)
-		{
-			_userManager = userManager;
-		}
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
 
-		public async Task<IActionResult> Index()
-		{
-			var users = _userManager.Users;
-			//Use AutoMapper in Future
-			IEnumerable<UserDTO> usersDTO = await users.Select(t => new UserDTO
-			{
-				Id = t.Id,
-				Login = t.UserName,
-				Email = t.Email,
-				YearOfBirth = t.YearOfBirth
-			}).ToListAsync();
-			return View(usersDTO);
-		}
-	}
+        public async Task<IActionResult> Index()
+        {
+            var users = _userManager.Users;
+            //Use AutoMapper in Future
+            IEnumerable<UserDTO> usersDTO = await users.Select(t => new UserDTO
+            {
+                Id = t.Id,
+                Login = t.UserName,
+                Email = t.Email,
+                YearOfBirth = t.YearOfBirth
+            }).ToListAsync();
+            return View(usersDTO);
+        }
+
+        public async Task<IActionResult> Create()
+            => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateUserDTO userDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User
+                {
+                    Email = userDTO.Email,
+                    UserName = userDTO.Login,
+                    YearOfBirth = userDTO.YearOfBirth,
+                };
+                var result = await _userManager.CreateAsync(user, userDTO.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(userDTO);
+        }
+
+        public async Task<IActionResult> Edit(string? id)
+        {
+            if (id is null)
+            {
+                return NotFound();
+            }
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            //AutoMapper
+            EditUserDTO dto = new EditUserDTO
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Login = user.UserName
+            };
+            return View(dto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(EditUserDTO userDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(userDTO.Id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                user.UserName = userDTO.Login;
+                user.Email = userDTO.Email;
+                user.YearOfBirth = userDTO.YearOfBirth;
+                IdentityResult result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(userDTO);
+        }
+    }
 }
