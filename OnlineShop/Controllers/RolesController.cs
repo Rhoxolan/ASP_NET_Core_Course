ddadcf6eb237c113967289801eedda7f0a0c9946
Rhoxolan <pba17mail@gmail.com>
2023-03-30 23:42:10 +0300

    The Online Shop project from academy was supplemented

---
+++ OnlineShop/Controllers/RolesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data;
+
+namespace OnlineShop.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly UserManager<User> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public RolesController(UserManager<User> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(roleManager.Roles);
+        }
+    }
+}
