d5dcc27eea0bbe2af226a73a0a5ca344234264d0
Rhoxolan <pba17mail@gmail.com>
2023-03-31 12:15:49 +0300

    The Online Shop project from academy was supplemented

--- OnlineShop/Controllers/RolesController.cs
+++ OnlineShop/Controllers/RolesController.cs
@@ -5,33 +5,54 @@ using OnlineShop.Data;
 
 namespace OnlineShop.Controllers
 {
-    public class RolesController : Controller
-    {
-        private readonly UserManager<User> userManager;
-        private readonly RoleManager<IdentityRole> roleManager;
+	public class RolesController : Controller
+	{
+		private readonly UserManager<User> userManager;
+		private readonly RoleManager<IdentityRole> roleManager;
 
-        public RolesController(UserManager<User> userManager,
-            RoleManager<IdentityRole> roleManager)
-        {
-            this.userManager = userManager;
-            this.roleManager = roleManager;
-        }
+		public RolesController(UserManager<User> userManager,
+			RoleManager<IdentityRole> roleManager)
+		{
+			this.userManager = userManager;
+			this.roleManager = roleManager;
+		}
 
-        public async Task<IActionResult> Index()
-        {
-            return View(await roleManager.Roles.ToListAsync());
-        }
+		public async Task<IActionResult> Index()
+		{
+			return View(await roleManager.Roles.ToListAsync());
+		}
 
-        public IActionResult Create()
-        {
-            return View();
-        }
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(string name)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(string name)
+		{
+			if (!string.IsNullOrEmpty(name))
+			{
+				IdentityRole newRole = new(name);
+				IdentityResult result = await roleManager.CreateAsync(newRole);
+				if (result.Succeeded)
+				{
+					return RedirectToAction(nameof(Index));
+				}
+				else
+				{
+					foreach (var error in result.Errors)
+					{
+						ModelState.AddModelError(string.Empty, error.Description);
+					}
+				}
+			}
+			else
+			{
+
+				ModelState.AddModelError(string.Empty, "Role name isnt valid");
+			}
+			return View();
+		}
+	}
 }
