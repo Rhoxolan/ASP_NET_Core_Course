cdb1f6243ef3a0072ff1837019b5dc151edf73c1
Rhoxolan <pba17mail@gmail.com>
2023-04-01 15:07:31 +0300

    The Online Shop project from academy was supplemented

---
+++ OnlineShop/AutoMapperProfiles/RoleProfiles/ChangeRoleProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using OnlineShop.Data;
+using OnlineShop.Models.ViewModels.RolesViewModels;
+
+namespace OnlineShop.AutoMapperProfiles.RoleProfiles
+{
+	public class ChangeRoleProfile : Profile
+	{
+		public ChangeRoleProfile() => CreateMap<User, ChangeRoleViewModel>();
+	}
+}
