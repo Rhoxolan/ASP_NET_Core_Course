9932775578330dcab22db5ff063439dead95f9c0
Rhoxolan <pba17mail@gmail.com>
2023-04-04 17:08:40 +0300

    The Online Shop project from the academy was supplemented

--- OnlineShop/Controllers/ClaimsController.cs
+++ OnlineShop/Controllers/ClaimsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using OnlineShop.Data;
 using System.Data;
+using System.Security.Claims;
 
 namespace OnlineShop.Controllers
 {
@@ -30,8 +31,45 @@ namespace OnlineShop.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(string claimType, string claimValue)
         {
-            User user = await userManager.GetUserAsync(HttpContext.User);
-            throw new NotImplementedException();
+            User? user = await userManager.GetUserAsync(HttpContext.User);
+            if (user is not null)
+            {
+                Claim claim = new(claimType, claimValue, ClaimValueTypes.String);
+                var result = await userManager.AddClaimAsync(user, claim);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                Errors(result);
+                return View();
+            }
+            return RedirectToAction("Login", "Account");
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(string claimsInfo)
+        {
+            User? user = await userManager.GetUserAsync(HttpContext.User);
+            if (user is null)
+            {
+                return RedirectToAction(nameof(AccountController.Login), "Account");
+            }
+            string[] info = claimsInfo.Split(',');
+            var claims = await userManager.GetClaimsAsync(user);
+            Claim? claimForDelete = claims.FirstOrDefault(t => t.Type == info[0]
+            && t.Value == info[1] && t.ValueType == info[2]);
+            await userManager.RemoveClaimAsync(user, claimForDelete);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public void Errors(IdentityResult identityResult)
+        {
+            foreach(var error in identityResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
         }
     }
 }
