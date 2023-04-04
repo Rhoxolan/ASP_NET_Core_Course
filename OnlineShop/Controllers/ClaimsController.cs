6b8ab035bca241918a2c8fce56523a1d06fd691f
Rhoxolan <pba17mail@gmail.com>
2023-04-04 12:17:33 +0300

    The Online Shop project from the academy was supplemented

--- OnlineShop/Controllers/ClaimsController.cs
+++ OnlineShop/Controllers/ClaimsController.cs
@@ -20,5 +20,18 @@ namespace OnlineShop.Controllers
         {
             return View(User?.Claims);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(string claimType, string claimValue)
+        {
+            User user = await userManager.GetUserAsync(HttpContext.User);
+            throw new NotImplementedException();
+        }
     }
 }
