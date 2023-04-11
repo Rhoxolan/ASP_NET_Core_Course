a597cda36c8cb4372a19089c96ce2908506a2a6d
Rhoxolan <pba17mail@gmail.com>
2023-04-11 12:06:13 +0300

    The Online Shop project from the academy was supplemented

---
+++ OnlineShop/Models/Domain/Cart.cs
@@ -0,0 +1,26 @@
+﻿using OnlineShop.Data;
+
+namespace OnlineShop.Models.Domain
+{
+    public class Cart
+    {
+        List<CartItem> cartItems = new();
+
+        public IEnumerable<CartItem> CartItems
+        {
+            get
+            {
+                return cartItems;
+            }
+        }
+
+        public void AddToCart(Product product, int count)
+        {
+            CartItem? item = cartItems.FirstOrDefault(t => t.Product.Id == product.Id);
+            if(item is not null)
+            {
+                
+            }
+        }
+    }
+}
---
+++ OnlineShop/Models/Domain/CartItem.cs
@@ -0,0 +1,11 @@
+﻿using OnlineShop.Data;
+
+namespace OnlineShop.Models.Domain
+{
+    public class CartItem
+    {
+        public Product Product { get; set; } = default!;
+
+        public int Count { get; set; } = default;
+    }
+}
