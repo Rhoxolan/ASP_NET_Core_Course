3cb8e233deacf3422b04dd6cc4fe69632527a86f
Rhoxolan <pba17mail@gmail.com>
2023-04-13 12:03:33 +0300

    The Online Shop project from the Academy was supplemented

---
+++ OnlineShop/Infrastructure/ModelBinders/CartModelBinder.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using OnlineShop.Extensions;
+using OnlineShop.Models.Domain;
+
+namespace OnlineShop.Infrastructure.ModelBinders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        public async Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if(bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+            string sessionKey = "cart";
+            Cart? cart = null;
+            IEnumerable<CartItem>? cartItems = null; 
+            if(bindingContext.HttpContext.Session != null)
+            {
+                cartItems
+                    = bindingContext.HttpContext.Session.Get<IEnumerable<CartItem>>("cart");
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
