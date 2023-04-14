d8bd030bfddc1a4e16d54b3189068c578872b203
Rhoxolan <pba17mail@gmail.com>
2023-04-14 10:45:12 +0300

    The Online Shop project from the Academy was supplemented

--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -12,50 +12,50 @@ using OnlineShop.Models.ViewModels.AccountViewModels;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAutoMapper(typeof(CategoryProfile),
-    typeof(CreateCategoryProfile),
-    typeof(EditCategoryProfile),
-    typeof(ProductProfile),
-    typeof(CreateProductProfile),
-    typeof(EditProductProfile),
-    typeof(PhotoProfile),
-    typeof(CreatePhotoProfile),
-    typeof(EditPhotoProfile),
-    typeof(RegisterViewModel),
-    typeof(UserProfile),
-    typeof(CreateUserProfile),
-    typeof(EditUserProfile),
-    typeof(ChangePasswordProfile),
-    typeof(RoleProfile),
-    typeof(ChangeRoleProfile)
-    );
+	typeof(CreateCategoryProfile),
+	typeof(EditCategoryProfile),
+	typeof(ProductProfile),
+	typeof(CreateProductProfile),
+	typeof(EditProductProfile),
+	typeof(PhotoProfile),
+	typeof(CreatePhotoProfile),
+	typeof(EditPhotoProfile),
+	typeof(RegisterViewModel),
+	typeof(UserProfile),
+	typeof(CreateUserProfile),
+	typeof(EditUserProfile),
+	typeof(ChangePasswordProfile),
+	typeof(RoleProfile),
+	typeof(ChangeRoleProfile)
+	);
 
 // Add services to the container.
 var configurations = builder.Configuration;
 builder.Services.AddControllersWithViews(opt =>
 {
-    opt.ModelBinderProviders.Insert(0, new CartModelBinderProvider());
+	opt.ModelBinderProviders.Insert(0, new CartModelBinderProvider());
 });
 builder.Services.AddIdentity<User, IdentityRole>(opt =>
 {
-    // Есть вариант добавить пользовательский валидатор - https://stackoverflow.com/questions/47988003/how-to-customize-asp-net-identity-core-username-to-allow-special-characters-and
-    opt.User.AllowedUserNameCharacters += "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ ()";
-    opt.User.RequireUniqueEmail = false;
+	// Есть вариант добавить пользовательский валидатор - https://stackoverflow.com/questions/47988003/how-to-customize-asp-net-identity-core-username-to-allow-special-characters-and
+	opt.User.AllowedUserNameCharacters += "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ ()";
+	opt.User.RequireUniqueEmail = false;
 }).AddEntityFrameworkStores<ShopDbContext>();
 string connStr = builder.Configuration.GetConnectionString("shopDB");
 builder.Services.AddDbContext<ShopDbContext>(options => options.UseSqlServer(connStr));
 builder.Services.AddAuthentication().AddGoogle(options =>
 {
-    IConfigurationSection googleSection = configurations.GetSection("Authentication:Google");
-    options.ClientId = googleSection["ClientId"];
-    options.ClientSecret = googleSection["ClientSecret"];
+	IConfigurationSection googleSection = configurations.GetSection("Authentication:Google");
+	options.ClientId = googleSection["ClientId"];
+	options.ClientSecret = googleSection["ClientSecret"];
 });
 builder.Services.AddAuthorization(option =>
 {
-    option.AddPolicy("ApplicationPolicy", policy =>
-    {
-        policy.RequireClaim("App", "Online Shop");
-        policy.RequireRole("Admin", "Manager");
-    });
+	option.AddPolicy("ApplicationPolicy", policy =>
+	{
+		policy.RequireClaim("App", "Online Shop");
+		policy.RequireRole("Admin", "Manager");
+	});
 });
 //builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
@@ -65,9 +65,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -80,7 +80,19 @@ app.UseAuthorization();
 app.UseSession();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+	name: "productsDefault",
+	pattern: "{category}",
+	defaults: new { controller = "Home", action = "Index" }
+	);
+
+app.MapControllerRoute(
+	name: "productsPages",
+	pattern: "{category}/{page}",
+	defaults: new { controller = "Home", action = "Index" }
+	);
 
 app.Run();
